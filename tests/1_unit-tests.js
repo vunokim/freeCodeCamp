f668fbeb0c68c40a086eaa534462d4ab4cddb7b2
bskim <68986589+vunokim@users.noreply.github.com>
2024-02-19 11:19:29 +0900

    Delete tests/1_unit-tests.js

--- tests/1_unit-tests.js
+++
@@ -1,9 +0,0 @@
-const chai = require('chai');
-const assert = chai.assert;
-
-test('#isTrue, #isNotTrue', function() {
-assert.isTrue(true, 'This will pass with the boolean value true');
-assert.isNotTrue('true', 'This will NOT pass with the string value "true"');
-assert.isTrue(1, 'This will NOT pass with the number value 1');
-  );
-});
